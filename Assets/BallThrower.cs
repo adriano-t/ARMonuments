e5c0bcf766b4f7d29b5a7002ddaace6de9e12443
Adriano Tumminelli <adriano.tummi@hotmail.it>
2020-08-21 10:29:31 +0200

    first commit

---
+++ Assets/BallThrower.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallThrower : MonoBehaviour
+{
+    public GameObject prefab;
+    public Transform modelTarget;
+    public float force = 10;
+    public Text label;
+    private void Update ()
+    {
+        if (Input.GetMouseButtonDown(0) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            ThrowBall();
+            label.gameObject.SetActive(true);
+        }
+        
+    }
+        
+    void ThrowBall()
+    {
+        GameObject obj = Instantiate(prefab, transform.position, transform.rotation);
+        obj.transform.SetParent(modelTarget, true);
+        Rigidbody rb = obj.GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * force, ForceMode.Impulse);
+        Physics.gravity = -10 * modelTarget.transform.up;
+    }
+}
---
+++ Assets/DestroyAfter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfter : MonoBehaviour
+{
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, Camera.main.transform.position) > 100)
+            Destroy(gameObject);
+    }
+}
