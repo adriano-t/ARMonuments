1e8d0d9a1caeba028545e99895be897d482139b3
Adriano Tumminelli <adriano.tummi@hotmail.it>
2020-09-16 16:49:14 +0200

    scene + prefab

--- Assets/AppManager.cs
+++ Assets/AppManager.cs
@@ -9,12 +9,16 @@ using Vuforia;
 
 public class AppManager : MonoBehaviour
 {
+    public Text statusLabel;
+    public Text statusInfoLabel;
     public Text debugLabel;
     public Text monumentLabel;
     public Text foundLabel;
     public Monument[] monuments;
     public float range = 0.001f;
     private string currentDataset;
+    private List<TrackableBehaviour> toDestroy = new List<TrackableBehaviour>();
+    private List<ModelTargetBehaviour> modelTargets = new List<ModelTargetBehaviour>();
     //public Text label;
     //public GameObject prefab;
     //public Transform modelTarget;
@@ -131,15 +135,15 @@ public class AppManager : MonoBehaviour
                         monumentLabel.text = "Monumento: " + monument.name + " (" + dist + "°)";
                         if (currentDataset != monument.dataset)
                         {
-                            //if (LoadAndActivateDataset(monument.dataset))
+                            if (LoadAndActivateDataset(monument))
                             {
+
                                 currentDataset = monument.dataset;
-                                monument.obj.SetActive(true);
                                 found = true;
                                 break;
                             }
-                            //else
-                            //    debugLabel.text += "Impossible to load dataset: " + monument.dataset + "\n";
+                            else
+                                debugLabel.text += "Impossible to load dataset: " + monument.dataset + "\n";
                         }
                         else
                         {
@@ -150,7 +154,6 @@ public class AppManager : MonoBehaviour
                     else
                     {
                         debugLabel.text +=  monument.name + " too far.\n";
-                        monument.obj.SetActive(false);
                     }
                 }
 
@@ -174,40 +177,104 @@ public class AppManager : MonoBehaviour
     //}
 
 
-    public bool LoadAndActivateDataset (string loadThisDataset)
-    { 
-        ObjectTracker objectTracker = TrackerManager.Instance.GetTracker<ObjectTracker>();
-        //Stop the tracker.
-        objectTracker.Stop();
+    public bool LoadAndActivateDataset (Monument monument)
+    {
+        //destroy the previous model targets
+        foreach (var t in modelTargets)
+            Destroy(t.gameObject);
+        modelTargets.Clear();
 
+
+        var datasetName = monument.name;
+        ObjectTracker objectTracker = TrackerManager.Instance.GetTracker<ObjectTracker>();
+        Debug.LogWarning("Loading Dataset: " + datasetName);
         //Create a new dataset object.
         DataSet dataset = objectTracker.CreateDataSet();
         //Load and activate the dataset if it exists.
-        if (DataSet.Exists(loadThisDataset))
+        if (DataSet.Exists(datasetName))
         {
-            if (!dataset.Load(loadThisDataset))
-            {
+            if (!dataset.Load(datasetName))
+            {            
                 debugLabel.text += "Load fail\n";
+                Debug.LogError("Load Fail");
                 return false;
             }
 
-            objectTracker.ActivateDataSet(dataset);
-            //if (!objectTracker.ActivateDataSet(dataset))
-            //{
-            //    debugLabel.text += "Activate fail\n";
-            //    return false;
-            //}
+
+            //Stop the tracker.
+            objectTracker.Stop();
+
+            //objectTracker.ActivateDataSet(dataset);
+            if (!objectTracker.ActivateDataSet(dataset))
+            {
+                debugLabel.text += "Activate fail\n";
+                Debug.LogError("Activate Fail");
+                return false;
+            }
         }
         else
         {
             debugLabel.text += "DataSet does not exists\n";
+            Debug.LogError("DataSet does not exists");
             return false;
         }
 
+
         //Start the object tracker.
-        return objectTracker.Start();
+        bool started = objectTracker.Start();
+        if (started)
+        {
+
+            IEnumerable<TrackableBehaviour> tbs = TrackerManager.Instance.GetStateManager().GetTrackableBehaviours();
+            toDestroy.Clear();
+            foreach (TrackableBehaviour tb in tbs)
+            {
+                if (tb is ModelTargetBehaviour)
+                {
+                    // add additional script components for trackable
+                    tb.gameObject.name = tb.TrackableName;
+                    tb.gameObject.AddComponent<DefaultTrackableEventHandler>();
+                    tb.gameObject.AddComponent<TurnOffBehaviour>();
+                    var msm = tb.gameObject.AddComponent<MonumentStatusManager>();
+                    msm.statusLabel = statusLabel;
+                    msm.statusInfoLabel = statusLabel;
+
+                    //spawn the corresponding augmentation object and attach to the trackable
+                    var augmentationObject = GetAumentationObject(monument, tb.TrackableName);
+                    augmentationObject.transform.SetParent(tb.gameObject.transform, false);
+
+                    modelTargets.Add(tb as ModelTargetBehaviour);
+                }
+                else
+                    toDestroy.Add(tb);//destroy this target (usually it spawns an image target)
+            }
+
+            //destroy all unwanted targets
+            foreach(var obj in toDestroy)
+                Destroy(obj.gameObject);
+            toDestroy.Clear();
+        }
+        Debug.Log(started ? "Tracker started" : "Tracked failed to start");
+        return started;
+    }
+
+    /// <summary>
+    /// returns a copyof the augmentation object
+    /// </summary>
+    /// <param name="monument"></param>
+    /// <param name="trackableName"></param>
+    /// <returns></returns>
+    private GameObject GetAumentationObject(Monument monument, string trackableName)
+    {
+        foreach(var mt in monument.targets)
+        {
+            if (mt.modelTarget == trackableName)
+                return Instantiate(mt.prefab, Vector3.zero, Quaternion.identity);
+        }
+        return null;
     }
 }
+
 
 [System.Serializable]
 public struct Monument
@@ -215,5 +282,12 @@ public struct Monument
     public string name;
     public Vector2 position;
     public string dataset;
-    public GameObject obj;
+    public MonumentTarget[] targets;
+}
+
+[System.Serializable]
+public struct MonumentTarget
+{
+    public string modelTarget;
+    public GameObject prefab;
 }
